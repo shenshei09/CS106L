f0b25fee6de03249c95b0332dbe077fef75a584d
lisin <1960572479@qq.com>
2022-11-04 14:57:24 +0800

    correct mistake on at()

--- cs106L-assignment3/hashmap.cpp
+++ cs106L-assignment3/hashmap.cpp
@@ -41,12 +41,17 @@ inline size_t HashMap<K, M, H>::bucket_count() const {
 }
 
 template<typename K, typename M, typename H>
-M &HashMap<K, M, H>::at(const K &key) const {
+M &HashMap<K, M, H>::at(const K &key) {
     auto [prev, node_found] = find_node(key);
     if (node_found == nullptr) {
         throw std::out_of_range("HashMap<K, M, H>::at: key not found");
     }
     return node_found->value.second;
+}
+
+template<typename K, typename M, typename H>
+const M &HashMap<K, M, H>::at(const K &key) const {
+    return static_cast<M &>(const_cast<HashMap<K, M, H> *>(this)->at(key));
 }
 
 template<typename K, typename M, typename H>
